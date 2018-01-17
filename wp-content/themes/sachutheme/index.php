ab544ee3bc01e32f5130788d6676be49417fda6e
Saraswathi Rajasekharan <saraswathi.rajan2@gmail.com>
2018-01-16 23:55:11 -0500

    addition of new post

--- wp-content/themes/sachutheme/index.php
+++ wp-content/themes/sachutheme/index.php
@@ -15,13 +15,13 @@
 			    </ol>
 				    <div class="carousel-inner">
 				      <div class="item carousel-image active" id="elem1">
-				        <img class ="post-image1" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/birds-original.jpg">
+				        <img class ="post-image1" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/ikea-logo-building-exterior-carousel.jpg">
 				      </div>
 				      <div class="item carousel-image" id="elem2">
-				       <img class ="post-image2" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/target-slider-new-years-resolution.jpg">
+				       <img class ="post-image2" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/holiday-blues-carousel.jpg">
 				      </div>
 				      <div class="item carousel-image" id="elem3">
-				       <img class ="post-image3" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/holiday-blues-carousel.jpg">
+				       <img class ="post-image3" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/birds-original.jpg">
 				      </div>
 				    </div>
 				     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
@@ -37,34 +37,34 @@
 		<div class="col-md-12 col-sm-12 col-xs-12 col-lg-6 carousel-preview">
 			<div class="row carousel-content1" id="carousel-content1">
 				<a class="btn tag" href="/lifestyle" role="button">Lifestyle</a>
-				<div class="post-title">Negativity and dealing with “negative people”</div>
-				<p class="latest-preview">A few months ago I hit rock bottom. I was fighting with my husband on a regular basis, I spent most of my time just sitting and watching tv or playing games, and I didn’t care to clean my apartment or eat well. The cause - my mother’s death - was a lone circumstance that snowballed every single ugliness under my skin and threatened to take down any good that lay in it's way...
+				<div class="post-title">Charm and Character in an Ikea Furnished Home</div>
+				<p class="latest-preview">Ikea is the furniture Goddess (or God) you go to when you need affordable, Scandinavian style furniture. Or, if you’re anything like me, Ikea is the land of decor inspiration and cheap meals.Ikea is extremely popular for their affordable, modern furniture selection.
+				Sadly, Ikea can also be easily passed off as a one stop shop for 3S’s - simple, standard and single use. People are increasingly on the lookout for...
 				</p>
-				<a class="btn link" href="/negativity-and-dealing-with-negative-people/" role="button">Continue Reading...</a>
+				<a class="btn link" href="/charm-character-ikea-furnished-home/" role="button">Continue Reading...</a>
 			</div>
 			<div class="row carousel-content2" id="carousel-content2">
 				<a class="btn tag" href="/lifestyle/" role="button">Lifestyle</a>
-				<div class="post-title">A New Year - Resolutions and Goal Setting</div>
-				<p class="latest-preview">
-											It's that time of the year when people start listing New Years Resolutions. I for one, have a giant list of goals I am determined to see through next year. The most important one? I struggle with continuity. I always find myself digging around for motivation when it fails me after a brief burst of creativity. This flaw has tyrannized most of my projects, and has threatened a lot of my creative drive.I want to end it...</p>	
-				<a class="btn link" href="/a-new-year-resolutions-goal-setting/" role="button">Continue Reading...</a>
+				<div class="post-title">Beat the Holiday Blues With These Tips</div>
+				<p class="latest-preview">Ever felt like you no longer have a purpose to serve after the holiday season is over? My mother would often use the term ‘escapism’ to describe a condition wherein people neglect realistic struggles for a life filled with dream like elements. The holidays is the epitome of this surreal world we aim to be a part of. Today’s commercialization of the holidays provides an even steadier foundation to customize your dream state...</p>	
+				<a class="btn link" href="/beat-the-holiday-blues-with-these-tips/" role="button">Continue Reading...</a>
 			</div>
 			<div class="row carousel-content3" id="carousel-content3">
-				<a class="btn tag" href="/wordpress/lifestyle" role="button">Lifestyle</a>
-				<div class="post-title">Beat the Holiday Blues With These Tips</div>
-					<p class"latest-preview">Ever felt like you no longer have a purpose to serve after the holiday season is over? My mother would often use the term ‘escapism’ to describe a condition wherein people neglect realistic struggles for a life filled with dream like elements. The holidays is the epitome of this surreal world we aim to be a part of. Today’s commercialization of the holidays provides an even steadier foundation to customize your dream state...</p>
+				<a class="btn tag" href="/lifestyle" role="button">Lifestyle</a>
+				<div class="post-title">Negativity and dealing with “negative people”</div>
+					<p class"latest-preview">A few months ago I hit rock bottom. I was fighting with my husband on a regular basis, I spent most of my time just sitting and watching tv or playing games, and I didn’t care to clean my apartment or eat well. The cause - my mother’s death - was a lone circumstance that snowballed every single ugliness under my skin and threatened to take down any good that lay in it's way...</p>
 					<br>
-				<a class="btn link" href="/beat-the-holiday-blues-with-these-tips/" role="button">Continue Reading...</a>
+				<a class="btn link" href="/negativity-and-dealing-with-negative-people/" role="button">Continue Reading...</a>
 			</div>
 			<div class="row carousel-thumbnail">
 				<div class="col-md-4 col-xs-4">
-					<img id="car-img-1" class="img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/birds.jpg">
+					<img id="car-img-1" class="img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/ikea-logo-building-exterior-thumbnail.jpg">
 				</div>
 				<div class="col-md-4 col-xs-4">
-					<img id="car-img-2" class="img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/target-new-years-resolution-thumb.jpg">	
+					<img id="car-img-2" class="img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/holiday-blues-carousel-thumbnail.jpg">	
 				</div>
 				<div class="col-md-4 col-xs-4">
-					<img id="car-img-3" class="img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/holiday-blues-carousel-thumbnail.jpg">	
+					<img id="car-img-3" class="img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/birds.jpg">	
 				</div>
 			</div>
 		</div>
@@ -138,6 +138,17 @@
 					</a>
 				</div>
 				<div class="col-sm-6 col-xs-6 popular-posts">
+					<a href="/charm-character-ikea-furnished-home/" class="popular-preview-mobile">
+					<img class="thumb-mobile img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/ikea-logo-building-exterior-post-preview-homepage.jpg">
+					</a>
+					<img class="thumb img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/ikea-logo-building-exterior-post-preview-homepage.jpg">
+					<a href="/charm-character-ikea-furnished-home/" class="popular-preview">
+						<div class="middle">
+						<div class="post-prev">Ikea is the furniture Goddess (or God) you go to when you need affordable, Scandinavian style furniture. Or, if you’re anything like me, Ikea is the land of...</div>
+						</div>
+					</a>
+				</div>
+				<!--div class="col-sm-6 col-xs-6 popular-posts">
 					<a href="#" class="popular-preview-mobile">
 					<img class="thumb-mobile img-responsive" src="https://s3.us-east-2.amazonaws.com/meanderingpaths/images/coming-soon-post-homepage.png">
 					</a>
@@ -149,7 +160,7 @@
 							Stay tuned for new content! Better yet, sign up for email updates! </div>
 						</div>
 					</a>
-				</div>
+				</div-->
 			</div>
 		</div>
 		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 about-panel">
